105451978c939456d8b7f98c2d370d7ef812f52d
nehadesai1204 <126406727+nehadesai1204@users.noreply.github.com>
2025-02-24 17:52:05 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12234
+let accountEmail = "xyz@gmail.com"
+var accountPassword ="1212"
+accountCity = "Jaipur"
+let accountState ;
+
+console.table([accountEmail , accountId , accountPassword , accountCity , accountState])
